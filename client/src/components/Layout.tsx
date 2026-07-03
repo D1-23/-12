18d0113b9fb6d446de2158a043e6575db89eb866
Haibao Dong (EXT,Hongyin,VENDOR) [7623683985687710684] <haibao.dong-ext@yanfeng.com>
2026-07-03 15:03:01 +0800

    初始化边栏插件布局和主题

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
-    <div className="w-screen h-screen">
+    <div className="h-screen w-full max-w-[360px] mx-auto bg-background flex flex-col overflow-hidden">
       <Outlet />
     </div>
   );
---
+++ client/src/pages/PrintWorkbench/PrintWorkbench.tsx
@@ -0,0 +1,12 @@
+import { PagePlaceholder } from '@lark-apaas/client-toolkit/components/PagePlaceholder';
+
+const PrintWorkbench = () => {
+  return (
+    <PagePlaceholder
+      title="排版打印边栏工作台"
+      description="飞书多维表格排版打印工具"
+    />
+  );
+};
+
+export default PrintWorkbench;
